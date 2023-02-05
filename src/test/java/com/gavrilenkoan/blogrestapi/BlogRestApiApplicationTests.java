75e77a68a8c339ac5316e15ee2c9fb0110e4236f
gavrilenko_an <havrilenko.ann@gmail.com>
2023-02-05 20:50:48 +0200

    Initial commit

---
+++ src/test/java/com/gavrilenkoan/blogrestapi/BlogRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gavrilenkoan.blogrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
