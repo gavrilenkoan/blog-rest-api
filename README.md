1e1ebb35425f58c4b22996430f76c6b358ce60c4
gavrilenko_an <havrilenko.ann@gmail.com>
2023-02-06 14:47:09 +0200

    add DatasourceConfig

---
+++ README.md
@@ -0,0 +1,3 @@
+## Database diagram
+
+[![blog_db_diagram.png](https://i.postimg.cc/13BZMyNS/blog-db-diagram.png)](https://postimg.cc/ZWWg5tcs)
---
+++ src/main/resources/db/migration/V1__InitTables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE _user (
+   id BIGSERIAL PRIMARY KEY,
+   username VARCHAR(50) NOT NULL,
+   firstname VARCHAR(50),
+   lastname VARCHAR(50),
+   email VARCHAR(50) NOT NULL,
+   "password" VARCHAR(50) NOT NULL,
+   unique (username, email)
+);
