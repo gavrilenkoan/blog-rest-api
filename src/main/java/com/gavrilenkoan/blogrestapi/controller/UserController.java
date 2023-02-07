83e2ccbfe8e123fd5b484062d16bf693c2406f26
gavrilenko_an <havrilenko.ann@gmail.com>
2023-02-07 19:15:54 +0200

    add "api/v1/user" routing

---
+++ src/main/java/com/gavrilenkoan/blogrestapi/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.gavrilenkoan.blogrestapi.controller;
+
+import com.gavrilenkoan.blogrestapi.config.JwtService;
+import com.gavrilenkoan.blogrestapi.dto.UserDto;
+import com.gavrilenkoan.blogrestapi.entity.User;
+import com.gavrilenkoan.blogrestapi.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RestController
+@RequestMapping(path  = "api/v1/user")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getAuthenticatedUser(HttpServletRequest request) {
+        String token = request.getHeader(AUTHORIZATION).substring("Bearer ".length());
+        return ResponseEntity.ok(userService.getUser(jwtService.extractId(token)));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Integer id) {
+        return ResponseEntity.ok(userService.getUser(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(HttpServletRequest request, @RequestBody UserDto userDto) {
+        String token = request.getHeader(AUTHORIZATION).substring("Bearer ".length());
+        return ResponseEntity.ok(userService.updateUser(jwtService.extractId(token), userDto));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(HttpServletRequest request) {
+        String token = request.getHeader(AUTHORIZATION).substring("Bearer ".length());
+        return ResponseEntity.ok(userService.deleteUser(jwtService.extractId(token)));
+    }
+
+}
---
+++ src/main/java/com/gavrilenkoan/blogrestapi/service/UserService.java
@@ -0,0 +1,52 @@
+package com.gavrilenkoan.blogrestapi.service;
+
+import com.gavrilenkoan.blogrestapi.das.UserDas;
+import com.gavrilenkoan.blogrestapi.dto.UserDto;
+import com.gavrilenkoan.blogrestapi.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserDas userDas;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public List<User> getAllUsers() {
+        return userDas.selectAllUsers();
+    }
+
+    public User getUser(Integer id) {
+        return userDas.selectUserById(id)
+                .orElseThrow(() -> new IllegalStateException("user with id " + id + "not found"));
+    }
+
+    public User updateUser(Integer id, UserDto userDto) {
+        if (userDto.getUsername() != null && !Objects.equals(userDto.getUsername(), "") && userDas.selectUserByUsername(userDto.getUsername()).isEmpty()) {
+            userDas.updateUserUsername(id, userDto.getUsername());
+        }
+        if (userDto.getFirstname() != null && !Objects.equals(userDto.getFirstname(), "")) {
+            userDas.updateUserFirstname(id, userDto.getFirstname());
+        }
+        if (userDto.getLastname() != null && !Objects.equals(userDto.getLastname(), "")) {
+            userDas.updateUserLastname(id, userDto.getLastname());
+        }
+        if (userDto.getPassword() != null && !Objects.equals(userDto.getPassword(), "")) {
+            userDas.updateUserPassword(id, bCryptPasswordEncoder.encode(userDto.getPassword()));
+        }
+        if (userDto.getEmail() != null && !Objects.equals(userDto.getEmail(), "") && userDas.selectUserByEmail(userDto.getEmail()).isEmpty()) {
+            userDas.updateUserEmail(id, userDto.getEmail());
+        }
+        return userDas.selectUserById(id)
+                .orElseThrow(() -> new IllegalStateException("user with id " + id + "not found"));
+    }
+
+    public String deleteUser(Integer id) {
+        return userDas.deleteUser(id) == 1 ? "deleted successfully" : "smth went wrong";
+    }
+}
